db33e634e32c6dd16023cc95ef62f5ebe200d4b3
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2025-12-14 09:49:18 +0500

    update Payment

--- src/payment.js
+++ src/payment.js
@@ -2,24 +2,31 @@ import css from "./style.css?raw";
 import template from "./template.html?raw";
 
 export class Payment {
-	#amount = 0;
+	#details = {
+		amount: 0,
+		merchant: "Pinkork (SPAIM)",
+		terminal: "358242410-1",
+		order: "00094617z424",
+		date: "12/12/2025 16:48",
+		productDescription: "1 Funko Mashle: Magic and Muscles Lemon Irvine 21...",
+	};
+
 	/** @type {string} */
 	#template = template;
 	/** @type {ShadowRoot | null} */
 	#shadow = null;
 
+	/** @param {number} amount */
 	constructor(amount) {
-		console.log(amount);
-		this.#amount = this.#sanitizeAmount(amount);
-		this.#insertAmountInTemplate();
+		this.#details.amount = amount;
 	}
 
 	renderPage() {
 		const host = document.createElement("div");
 		document.body.appendChild(host);
 
-		const shadow = host.attachShadow({ mode: "closed" });
-		shadow.innerHTML = this.#template;
+		this.#shadow = host.attachShadow({ mode: "closed" });
+		this.#shadow.innerHTML = this.#insertValuesInTemplate();
 		this.#applyStyles();
 	}
 
@@ -41,19 +48,15 @@ export class Payment {
 		this.#shadow.appendChild(style);
 	}
 
-	/**
-	 * @param {number} amount
-	 * @returns {number}
-	 */
-	#sanitizeAmount(amount) {
-		const n = Number(amount);
-
-		if (Number.isNaN(n) || !Number.isFinite(n) || n < 0)
-			throw new Error("Invalid amount");
-		return +n.toFixed(2);
-	}
-
-	#insertAmountInTemplate() {
-		return this.#template.replace("__AMOUNT__", this.#amount);
+	#insertValuesInTemplate() {
+		// можно заменить на поиск по __([A-Z0-9_]+)__ и динамическую подстановку,
+		//   чтобы не производить много replace
+		return this.#template
+			.replace("__AMOUNT__", this.#details.amount.toFixed(2))
+			.replace("__MERCHANT__", this.#details.merchant)
+			.replace("__TERMINAL__", this.#details.terminal)
+			.replace("__ORDER__", this.#details.order)
+			.replace("__DATE__", this.#details.date)
+			.replace("__PRODUCT_DESCRIPTION__", this.#details.productDescription);
 	}
 }
